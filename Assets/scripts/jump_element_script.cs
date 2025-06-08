0fc79d719eaa4e91ec6e33bc790895cef5c9d1a6
Jaspher Fenix <jaspher.fenix081004@gmail.com>
2025-06-08 19:44:04 +0800

    add jump function to jump_element

--- Assets/scripts/jump_element_script.cs
+++ Assets/scripts/jump_element_script.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class bounce_element_script : MonoBehaviour
 {
+    public Rigidbody2D jump_element_rigid_body;
+    public float jump_height;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump_element_rigid_body.linearVelocity = Vector2.up * jump_height;
+        }
     }
 }
